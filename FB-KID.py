17936504c2e5b2717b5e7f41a7d253b4e083fe0c
SEXY0090 <111700205+SEXY0090@users.noreply.github.com>
2022-10-27 08:20:31 +0600

    Add files via upload

---
+++ FB-KID.py
@@ -0,0 +1,10 @@
+import os
+os.system('clear')
+os.system("xdg-open https://www.facebook.com/ITX.AK.SPIDER.10100")
+print('\n Fuck FB-KID My Son')
+print('\n [1] Fuck Max version (new)\n [2] Fuck pro version (old)')
+fuck=input('\n Choose for fuck: ')
+if fuck in ['1','01']:
+  import Fuck
+else:
+  os.system('python lol.py')
